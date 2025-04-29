a263a158ba71711667ba570e9171135e1201d3e0
YashvantBankapur2020 <yashvantbankapur@gmail.com>
2025-04-29 09:31:14 +0530

    File is created

--- java29/Employee29.java
+++ java29/Employee29.java
@@ -3,12 +3,14 @@ public class Employee29 {
  String name;
  int id;
  double salary;
+ String address;
 
- public Employee29(String name, int id, double salary) 
+ public Employee29(String name, int id, double salary, String address )
  {
     this.name = name;
     this.id = id;
     this.salary = salary;
+    this.address = address;
  }
  
  public void display()
@@ -16,11 +18,11 @@ public class Employee29 {
     System.out.println("Employee Name:"+name);
     System.out.println("Employee ID:"+id);
     System.out.println("Employee Salary:"+salary);
-
+    System.out.println("Employee address:"+address);
  }
 
  public static void main(String[] args) {
-    Employee29 emp=new Employee29("Yashvant", 264,50000);
+    Employee29 emp=new Employee29("Yashvant", 264,50000,"vidhya nagar");
     emp.display();
  }
  
